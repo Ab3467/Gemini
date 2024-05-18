2a00b70afedf802be7412c1a1e38d09cb4101c6a
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-18 12:18:32 +0500

    changes

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
-    <div className=" bg-slate-500">
-      Hello
+    <div>
+     <Navbar/>
       </div>
   );
 }
