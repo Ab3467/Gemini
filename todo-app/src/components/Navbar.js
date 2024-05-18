5d148b84c05feb67c923e03f1ef71436fc574f58
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-18 12:22:11 +0500

    changes

--- todo-app/src/components/Navbar.js
+++ todo-app/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar(props) {
   return (
-    <div>
+    <div className=' text-gray-50 bg-gray-900 flex flex-1 justify-center items-center m-2'>
       {props.Navbar}
     </div>
   )
