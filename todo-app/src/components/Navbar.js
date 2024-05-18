612c96d7f425dac4d98096e0d8d852e563c2d19d
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-18 12:32:21 +0500

    changes

--- todo-app/src/components/Navbar.js
+++ todo-app/src/components/Navbar.js
@@ -3,11 +3,12 @@ import React from 'react'
 export default function Navbar(props) {
   return (
     <nav className=' flex mx-auto'>
+        <div className='logo'>
+            <span className='justify-between flex bg-slate-800 text-slate-200'>Tasks</span>
+        </div>
         <ul>
             <li>Home</li>
-            <li>About</li>
-            <li></li>
-            <li></li>
+            <li>Youe Tasks</li>
         </ul>
     </nav>
   )
