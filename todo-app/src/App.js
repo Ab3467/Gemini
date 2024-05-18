fb3c0b167993c58e5e893f4d36c3810d46838b3d
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-18 12:52:03 +0500

    changes

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,9 +4,12 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-     <Navbar/>
-     </div>
+    <>
+      <Navbar />
+      <div className=" bg-violet-100 p-5 rounded-md my-5 mx-auto container">
+         <h1 className=" text-xl font-bold">Your Todos</h1>
+      </div>
+    </>
   );
 }
 
