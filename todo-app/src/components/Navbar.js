60605ef153a98998f95c9d0c5b974aa1ee4f327e
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-18 12:41:12 +0500

    changes

--- todo-app/src/components/Navbar.js
+++ todo-app/src/components/Navbar.js
@@ -1,15 +1,13 @@
 import React from 'react'
 
 export default function Navbar(props) {
-  return (
-    <nav className='justify-between flex bg-indigo-900 text-white py-2'>
-        <div className='logo'>
+    return (
+        <nav className='justify-between flex bg-indigo-900 text-white py-2'>
             <span className=' font-bold text-xl mx-8'>Tasks</span>
-        </div>
-        <ul className='flex gap-8 mx-9'> 
-            <li className=' cursor-pointer hover:font-bold transition-all'>Home</li>
-            <li className=' cursor-pointer hover:font-bold transition-all'>Youe Tasks</li>
-        </ul>
-    </nav>
-  )
+            <ul className='flex gap-8 mx-9'>
+                <li className=' cursor-pointer hover:font-bold transition-all'>Home</li>
+                <li className=' cursor-pointer hover:font-bold transition-all'>Youe Tasks</li>
+            </ul>
+        </nav>
+    )
 }
