f140cc3f70e773658ebcb986b4c8a6cc9818b2ac
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-20 17:57:12 +0500

    Changes

--- todo-app/src/components/Navbar.js
+++ todo-app/src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <>
     <nav>
-       <ul>
+       <ul className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200  hover:text-white'>
         <li>Home</li>
         <li>About</li>
        </ul>
