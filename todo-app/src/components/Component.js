4dbe523dba37c36d4f7b568f0564bade02746a31
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-20 18:15:57 +0500

    Changes

--- todo-app/src/components/Component.js
+++ todo-app/src/components/Component.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useContext } from 'react'
+import { counterContext } from '../context/contextapi'
 
 export default function Component() {
+    const counter = useContext(counterContext)
   return (
     <div>
-      Component
+      {counter}
     </div>
   )
 }
