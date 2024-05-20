0141e51a13d81690023dfbacce7ec4dec59447e2
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-20 17:52:07 +0500

    Changes

---
+++ todo-app/src/components/Component.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Component() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- todo-app/src/components/Navbar.js
+++ todo-app/src/components/Navbar.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import Button from './Button'
 
 export default function Navbar() {
   return (
+    <>
     <nav>
        <ul>
         <li>Home</li>
         <li>About</li>
        </ul>
     </nav>
+    <Button/>
+    </>
   )
 }
