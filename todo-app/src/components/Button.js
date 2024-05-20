e6d1129a72844ab48056fbec9eac10dcbc6951ad
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-20 17:53:22 +0500

    Changes

--- todo-app/src/components/Button.js
+++ todo-app/src/components/Button.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Component from './Component'
 
 export default function Button() {
   return (
     <div>
-      
+      <button><Component/>Count</button>
     </div>
   )
 }
