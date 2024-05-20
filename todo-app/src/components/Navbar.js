7065a8fc2e3961c566e533333c94416887704016
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-20 18:02:26 +0500

    Changes

--- todo-app/src/components/Navbar.js
+++ todo-app/src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <>
     <nav>
-       <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-grey-800 text-grey-500'>
+       <ul className='flex p-4 items-center m-3 gap-5'>
         <li>Home</li>
         <li>About</li>
        </ul>
---
+++ todo-app/src/context/contextapi.js
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import React from 'react'
+
+export default function contextapi() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
