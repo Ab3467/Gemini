53aeba1209d7a9403ec295eb2962b6c512e57b89
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-21 18:44:15 +0500

    Changes

--- gemini-app/src/components/sidebar/sidebar.js
+++ gemini-app/src/components/sidebar/sidebar.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import "./Sidebar.css"
+import "../../assets/assets.js"
 
 export default function Sidebar() {
   return (
     <div className='sidebar'>
-      
+      <div className="">
+        <img className='menu' src={assets.menu_icon} alt="" />
+        <div className="">
+
+        </div>
+      </div>
     </div>
   )
 }
