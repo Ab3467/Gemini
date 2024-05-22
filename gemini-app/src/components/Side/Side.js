9f70b6a9a75fcdd2186f8f02a8777a536d89d5e0
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-22 19:32:34 +0500

    Changes

--- gemini-app/src/components/Side/Side.js
+++ gemini-app/src/components/Side/Side.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import assets from "../../assets/assets.js"
+import {assets} from "../../assets/assets.js"
 
 export default function Side() {
   return (
@@ -10,6 +10,16 @@ export default function Side() {
           <img src={assets.plus_icon} alt="" />
           <p>New Chat</p>
           </div>
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+              <img src={assets.message_icon} alt="" />
+              <p>What is react ...</p>
+            </div>
+          </div>
+      </div>
+      <div className="bottom">
+
       </div>
     </div>
   )
