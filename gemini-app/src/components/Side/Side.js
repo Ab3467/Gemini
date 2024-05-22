e00774a864b3d595983eed2fdbfe1c6136f4b057
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-22 19:14:49 +0500

    Changes

--- gemini-app/src/components/Side/Side.js
+++ gemini-app/src/components/Side/Side.js
@@ -3,7 +3,13 @@ import React from 'react'
 export default function Side() {
   return (
     <div className='sidebar'>
-      
+      <div className='top'>
+        <img src={} alt="" className='menu'/>
+          <div className='new-chat'>
+          <img src={} alt="" />
+          <p>New Chat</p>
+          </div>
+      </div>
     </div>
   )
 }
