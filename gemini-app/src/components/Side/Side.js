3f48688cd90fc7dac63fdce45a71ee872c95b444
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-22 19:32:48 +0500

    Changes

--- gemini-app/src/components/Side/Side.js
+++ gemini-app/src/components/Side/Side.js
@@ -1,22 +1,22 @@
 import React from 'react'
-import {assets} from "../../assets/assets.js"
+import { assets } from "../../assets/assets.js"
 
 export default function Side() {
   return (
     <div className='sidebar'>
       <div className='top'>
-        <img src={assets.menu_icon} alt="" className='menu'/>
-          <div className='new-chat'>
+        <img src={assets.menu_icon} alt="" className='menu' />
+        <div className='new-chat'>
           <img src={assets.plus_icon} alt="" />
           <p>New Chat</p>
+        </div>
+        <div className="recent">
+          <p className="recent-title">Recent</p>
+          <div className="recent-entry">
+            <img src={assets.message_icon} alt="" />
+            <p>What is react ...</p>
           </div>
-          <div className="recent">
-            <p className="recent-title">Recent</p>
-            <div className="recent-entry">
-              <img src={assets.message_icon} alt="" />
-              <p>What is react ...</p>
-            </div>
-          </div>
+        </div>
       </div>
       <div className="bottom">
 
