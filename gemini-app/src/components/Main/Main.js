078f1cb0ae96213ddfa4128301883b6759114f49
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-23 20:30:28 +0500

    changing

--- gemini-app/src/components/Main/Main.js
+++ gemini-app/src/components/Main/Main.js
@@ -7,7 +7,19 @@ export default function Main() {
     <div className='main'>
       <div className="nav">
       <p>Gemini</p>
-        <img src={assets.gemini_icon} alt="" />
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+            <span>Hello,Div</span>
+            <p>How can i help you?</p>
+        </div>
+        <div className="cards">
+            <div className="card">
+                <p>Suggest beautiful places to see on an upcoming road trip</p>
+                <img src={assets.compass_icon} alt="" />
+            </div>
+        </div>
       </div>
     </div>
   )
