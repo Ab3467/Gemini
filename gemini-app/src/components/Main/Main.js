396468d7c4940c4797bf707713a23336662c0526
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-24 19:06:55 +0500

    changing

--- gemini-app/src/components/Main/Main.js
+++ gemini-app/src/components/Main/Main.js
@@ -35,11 +35,11 @@ export default function Main() {
         <div className="main-bottom">
           <div className="searchBox">
             <input type="text" name="" id="" placeholder='Enter prompt here' />
-          </div>
-          <div>
-            <img src={assets.gallery_icon} alt="" />
-            <img src={assets.mic_icon} alt="" />
-            <img src={assets.send_icon} alt="" />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
           </div>
           <p className='bottom-info'>
             Gemini may display inaccurate info,including about people,so double check its result please!
