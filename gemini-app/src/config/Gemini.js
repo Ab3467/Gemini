6ff93b0e36510cae61e625453128e10738e0166e
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-24 19:40:29 +0500

    changing

--- gemini-app/src/config/Gemini.js
+++ gemini-app/src/config/Gemini.js
@@ -48,7 +48,7 @@ const {
     },
   ];
   
-  async function run() {
+  async function run(prompt) {
     const chatSession = model.startChat({
       generationConfig,
       safetySettings,
@@ -56,8 +56,8 @@ const {
       ],
     });
   
-    const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
+    const result = await chatSession.sendMessage(prompt);
     console.log(result.response.text());
   }
   
-  run();
+  export default run;
