c86bc289e347a940890b7d6f45be590d143dd407
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-24 18:59:50 +0500

    changing

--- gemini-app/src/components/Main/Main.js
+++ gemini-app/src/components/Main/Main.js
@@ -6,38 +6,40 @@ export default function Main() {
   return (
     <div className='main'>
       <div className="nav">
-      <p>Gemini</p>
+        <p>Gemini</p>
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
         <div className="greet">
-            <span>Hello,Div</span>
-            <p>How can i help you?</p>
+          <span>Hello,Div</span>
+          <p>How can i help you?</p>
         </div>
         <div className="cards">
-            <div className="card">
-                <p>Suggest beautiful places to see on an upcoming road trip</p>
-                <img src={assets.compass_icon} alt="" />
-            </div>
-            <div className="card">
-                <p>Suggest beautiful places to see on an upcoming road trip</p>
-                <img src={assets.bulb_icon} alt="" />
-            </div>
-            <div className="card">
-                <p>Suggest beautiful places to see on an upcoming road trip</p>
-                <img src={assets.message_icon} alt="" />
-            </div>
-            <div className="card">
-                <p>Suggest beautiful places to see on an upcoming road trip</p>
-                <img src={assets.code_icon} alt="" />
-            </div>
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
         </div>
         <div className="main-bottom">
           <div className="searchBox">
-            <input type="text" name="" id="" placeholder='Enter prompt here'/>
+            <input type="text" name="" id="" placeholder='Enter prompt here' />
           </div>
           <div>
-            <img src={assets.gallery_icon} alt="" /><img src={assets.mic_icon} alt="" /><img src={assets.send_icon} alt="" />
+            <img src={assets.gallery_icon} alt="" />
+            <img src={assets.mic_icon} alt="" />
+            <img src={assets.send_icon} alt="" />
           </div>
           <p className='bottom-info'>
             Gemini may display inaccurate info,including about people,so double check its result please!
