877ef45f6b67ec85b23f74e649c38e440206cc44
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-24 20:11:02 +0500

    changing

--- gemini-app/src/context/Context.js
+++ gemini-app/src/context/Context.js
@@ -2,8 +2,18 @@ import { createContext } from "react";
 
 export const Context = createContext();
 
-const contextProvider = (props) =>{
-const contextValue = {
-    
+const ContextProvider = (props) =>{
+
+
+const ContextValue = {
+
 }
+
+return (
+    <Context.Provider value={ContextValue}>
+        {props.childern}
+    </Context.Provider>
+)
 } 
+
+export default ContextProvider
